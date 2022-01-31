46172e5c298036989d4d065a41ebb085bd46e5a3
M-S-Kashif <mskashif@deakin.edu.au>
2022-01-31 12:54:47 +0500

    Added two more pages for student account.

--- routes/student_router.js
+++ routes/student_router.js
@@ -7,18 +7,25 @@ const studentControllerLayer = require("../controller/student_controller_layer")
 
 //HOME API...
 router.get('/homepage', (req, res) => {
-    //res.send("<h1>Home Page from student router</h1>");
     res.render('./student/homepage.ejs');
 });
 
 //Contact Us API...
 router.get('/contact', (req, res) => {
-    //res.send("<h1>Home Page from student router</h1>");
     res.render('./student/contact.ejs');
 });
 
-//Blogs API...
+//FAQs API...
+router.get('/FAQs', (req, res) => {
+    res.render('./student/faqs.ejs');
+});
+
 //Quiz API...
+router.get('/Quiz', (req, res) => {
+    res.render('./student/quiz.ejs');
+});
+
+//Blogs API...
 //Quiz Result API...
 //About Us API...
 
